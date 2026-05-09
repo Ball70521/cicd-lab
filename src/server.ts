9fcbf4984627e20667c1e90b8b34e442d63a91f3
ball70521 <ball70521@gmail.com>
2026-05-09 17:52:25 +0800

    強制型別錯誤

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import { buildApp } from './app';
-
-const app = buildApp({ logger: true });           
+const errorTest: number = "I am a string";
+const app = buildApp({ logger: true });
 const port = Number(process.env.PORT || 3000);
 const host = process.env.HOST || '0.0.0.0';
 
 async function start() {
-try {
-await app.listen({ port, host });
-app.log.info(`Server listening at http://${host}:${port}`);
-} catch (error) {
-app.log.error(error);
-process.exit(1);
-}
+  try {
+    await app.listen({ port, host });
+    app.log.info(`Server listening at http://${host}:${port}`);
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
 }
 
 void start();
