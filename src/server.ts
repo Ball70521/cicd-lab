e756482427b8fa970c0d1d1bdc2095ef792955da
ball70521 <ball70521@gmail.com>
2026-05-09 17:46:45 +0800

    無縮排

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ const port = Number(process.env.PORT || 3000);
 const host = process.env.HOST || '0.0.0.0';
 
 async function start() {
-  try {
-    await app.listen({ port, host });
-    app.log.info(`Server listening at http://${host}:${port}`);
-  } catch (error) {
-    app.log.error(error);
-    process.exit(1);
-  }
+try {
+await app.listen({ port, host });
+app.log.info(`Server listening at http://${host}:${port}`);
+} catch (error) {
+app.log.error(error);
+process.exit(1);
+}
 }
 
 void start();
